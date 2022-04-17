c3c74723407433e80474b0aa7a47b1358c3a4214
Oural1206 <ouralpioupiou@gmail.com>
2022-04-17 18:23:42 +0200

    Create base

--- main.php
+++ main.php
@@ -1,1 +1,12 @@
+<?php
 
+function getUrl($url){
+    $session = curl_init($url); 
+    curl_setopt($session, CURLOPT_RETURNTRANSFER, true); 
+    $apiResponse = curl_exec($session);
+    curl_close($session);
+    return $apiResponse;
+};
+
+$configFile = file_get_contents("config.json");
+$config = json_decode($configFile, true);
