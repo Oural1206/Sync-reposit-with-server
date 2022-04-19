2255ff888dad602c9bbed3071709f33e0766d290
Oural1206 <ouralpioupiou@gmail.com>
2022-04-19 10:22:07 +0200

    Just download reposit

--- main.php
+++ main.php
@@ -1,12 +1,24 @@
 <?php
 
+$configFile = file_get_contents("config.json");
+$config = json_decode($configFile, true);
+
 function getUrl($url){
     $session = curl_init($url); 
     curl_setopt($session, CURLOPT_RETURNTRANSFER, true); 
+    curl_setopt($session, CURLOPT_USERAGENT, true); 
     $apiResponse = curl_exec($session);
+    $jsonResponse = json_decode($apiResponse, true);
     curl_close($session);
-    return $apiResponse;
+    return $jsonResponse;
 };
 
-$configFile = file_get_contents("config.json");
-$config = json_decode($configFile, true);
+
+$link = "https://github.com/". $config["username"]. "/". $config["repositoryName"]. "/archive/refs/heads/main.zip";
+$file = file_put_contents("repository.zip", file_get_contents($link));
+$zip = new ZipArchive;
+
+$zip -> open("repository.zip");
+$zip -> extractTo(".");
+$zip -> close();
+unlink("repository.zip");
